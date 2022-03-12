4b9d166ec16778e2ac0c1b0330175dc8051352f4
smjaye <sjayeson@gmail.com>
2022-03-11 18:56:41 -0800

    Change of code

--- welcome_friends.py
+++ welcome_friends.py
@@ -1,3 +1,3 @@
-'Hi friends, welcome to the digital world.'
-'How great that we are grouped here to learn something new, learn from each other.'
-'Lets learn.'
+print('Hi friends, welcome to the digital world.')
+print('How great that we are grouped here to learn something new, learn from each other.')
+print('Lets learn.')
